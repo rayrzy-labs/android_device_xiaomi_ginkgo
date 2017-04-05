b5472f324ffcbced81fe9c052853ec0fed5744b5
Jaekyun Seok <jaekyun@google.com>
2017-04-05 11:23:26 +0900

    ginkgo: Enforce RRO on framework-res
    Overlays only for framework-res will be converted into RROs.
    
    Other overlays can't be converted due to some known issues on app RRO.
    
    Bug: 36231603
    Change-Id: Idca30fdbbcc990fd124de16a06a112a346612a61
    Signed-off-by: rayrzy <vrayananda@yahoo.com>

--- device.mk
+++ device.mk
@@ -13,6 +13,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_ENFORCE_RRO_TARGETS := \
+    framework-res
+
 # AID/fs configs
 PRODUCT_PACKAGES += \
     fs_config_files
