0bcef3d9af2fc7722faf7a1d7b60fa409bd315b1
dianlujitao <dianlujitao@lineageos.org>
2019-10-18 11:05:40 +0800

    ginkgo: Enable QC framework value-adds
    Change-Id: I1987d3509b3db7cd4204537a6f2dc2ff4c611278
    Signed-off-by: rayrzy <vrayananda@yahoo.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -187,6 +187,7 @@ TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 TARGET_USES_INTERACTION_BOOST := true
 
 # Properties
+TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
